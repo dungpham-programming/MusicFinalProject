290367f61c203406f40ec5ccb090ee1ec68f0c87
dungpham-programming <21130326@st.hcmuaf.edu.vn>
2024-06-22 11:00:33 +0700

    Hoàn thiện HomeFrangment và inflate vào MainActivity

--- app/src/main/java/com/ltmobile/music/ui/MainActivity.java
+++ app/src/main/java/com/ltmobile/music/ui/MainActivity.java
@@ -1,28 +1,15 @@
 package com.ltmobile.music.ui;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ltmobile.music.R;
 import com.ltmobile.music.ui.fragment.HomeFragment;
-import com.ltmobile.music.util.RenderRecyclerComponent;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
